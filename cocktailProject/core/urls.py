f32e8dc627ba8801cd5d636373a526bf9e3508b7
Sammyd <samdolling@me.com>
2025-01-03 16:01:54 -0600

    tab component

--- cocktailProject/core/urls.py
+++ cocktailProject/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import dashboard_view
+from .views import dashboard_view, test_one, test_two, test_three
 
 urlpatterns = [
     path('',dashboard_view,name='dashboard'),
+    path('one/',test_one,name='one'),
+    path('two/',test_two,name='two'),
+    path('three/',test_three,name='three'),
 ]
--- cocktailProject/core/views.py
+++ cocktailProject/core/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard_view(request):
     return render(request, "dashboard.html")
+
+def test_one(request):
+    return render(request, "one.html")
+
+def test_two(request):
+    return render(request, "two.html")
+
+def test_three(request):
+    return render(request, "three.html")
