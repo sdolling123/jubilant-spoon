0aad8789b6e080a14c071752e1f36527c7ab14c0
Sammyd <samdolling@me.com>
2025-01-06 14:32:05 -0600

    delete working, next is messages

--- cocktailProject/core/urls.py
+++ cocktailProject/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CocktailListView, test_one, test_two, test_three
+from .views import CocktailListView,DeleteCocktailView, test_one, test_two, test_three
 
 urlpatterns = [
     path('',CocktailListView.as_view(),name='dashboard'),
+    path('delete/<int:pk>/',DeleteCocktailView.as_view(),name='delete'),
     path('one/',test_one,name='one'),
     path('two/',test_two,name='two'),
     path('three/',test_three,name='three'),
--- cocktailProject/core/views.py
+++ cocktailProject/core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DeleteView
+from django.contrib import messages
 from .models import Cocktail
 
 class CocktailListView(ListView):
@@ -18,6 +19,32 @@ class CocktailListView(ListView):
             for cocktail in self.get_queryset()
         ]
         return context
+    
+class DeleteCocktailView(DeleteView):
+    model = Cocktail
+    template_name = "partials/delete.html"  # Optional if no template is rendered
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add ingredients to the context
+        context['cocktails_with_ingredients'] = [
+            {
+                'cocktail': cocktail,
+                'ingredients': cocktail.ingredients.all()
+            }
+            for cocktail in self.get_queryset()
+        ]
+        return context
+
+    def delete(self, request, *args, **kwargs):
+        """Handle DELETE requests."""
+        self.object = get_object_or_404(Cocktail, pk=kwargs['pk'])
+        self.object.delete()
+        messages.success(request, f"The cocktail '{self.object.name}' was successfully deleted!")
+
+        # Prepare the context for the updated list
+        context = self.get_context_data()
+        return render(request, 'partials/card_list.html', context)
 
 def test_one(request):
     return render(request, "one.html")
